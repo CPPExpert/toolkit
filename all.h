e58860557f97e42a6b20e15774f2237e35f53c0b
Max Azoury <xxcooloppoxx@gmail.com>
2014-06-28 18:00:12 -0400

    Merged toolkit.h into all.h

--- all.h
+++ all.h
@@ -14,3 +14,17 @@
 #include <base64.h>
 
 #include <boost/crc.hpp>
+
+namespace toolkit
+{
+class CRC;
+class MD5;
+class SHA1;
+class SHA256;
+class BASE64;
+class wrapper;
+class func_s;
+class memory;
+class entity;
+class sigscan;
+};
